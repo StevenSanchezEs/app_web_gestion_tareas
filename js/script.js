5b763ac14bb47214937bf8320e918ee717adc458
Steve Sanchez <1steven.nathan.sanchez@gmail.com>
2024-05-30 16:48:24 -0600

    Update: plantillas

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//const apiUrl = 'https://apigestiontareas-production.up.railway.app';
+const apiUrl = 'https://apigestiontareas-production.up.railway.app';
 
 document.getElementById('login-form').addEventListener('submit', function (e) {
     e.preventDefault();
@@ -6,7 +6,7 @@ document.getElementById('login-form').addEventListener('submit', function (e) {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    fetch('https://apigestiontareas-production.up.railway.app/api/token/', {
+    fetch(`${apiUrl}/api/token/`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -60,7 +60,7 @@ document.getElementById('create-task-form').addEventListener('submit', function 
     }
 
 
-    fetch('https://apigestiontareas-production.up.railway.app/api/tasks/', {
+    fetch(`${apiUrl}/api/tasks/`, {
         method: 'POST',
         headers: {
             'Authorization': `Token ${token}`,
@@ -97,7 +97,7 @@ function fetchTasks() {
         return;
     }
 
-    fetch('https://apigestiontareas-production.up.railway.app/api/tasks/', {
+    fetch(`${apiUrl}/api/tasks/`, {
         method: 'GET',
         headers: {
             'Authorization': `Token ${token}`,
@@ -149,7 +149,7 @@ function fetchTasks(pageNumber = 1) { // Agrega un parámetro para el número de
         return;
     }
 
-    fetch(`https://apigestiontareas-production.up.railway.app/api/tasks/?page=${pageNumber}`, { // Incluye el número de página en la URL de la solicitud GET
+    fetch(`${apiUrl}/api/tasks/?page=${pageNumber}`, { // Incluye el número de página en la URL de la solicitud GET
         method: 'GET',
         headers: {
             'Authorization': `Token ${token}`,
@@ -227,7 +227,7 @@ function deleteTask(taskId) {
         return; // Si el usuario cancela, no hacemos nada
     }
 
-    fetch(`https://apigestiontareas-production.up.railway.app/api/tasks/${taskId}/`, {
+    fetch(`${apiUrl}/api/tasks/${taskId}/`, {
         method: 'DELETE',
         headers: {
             'Authorization': `Token ${token}`,
@@ -260,7 +260,7 @@ function editTask(taskId) {
 
     currentTaskId = taskId;
 
-    fetch(`https://apigestiontareas-production.up.railway.app/api/tasks/${taskId}/`, {
+    fetch(`${apiUrl}/api/tasks/${taskId}/`, {
         method: 'GET',
         headers: {
             'Authorization': `Token ${token}`,
@@ -314,7 +314,7 @@ document.getElementById('edit-task-form').addEventListener('submit', function(e)
     if (taskDescription) requestBody.descripcion = taskDescription;
     if (taskDate) requestBody.fecha_expiracion = taskDate;
 
-    fetch(`https://apigestiontareas-production.up.railway.app/api/tasks/${currentTaskId}/`, {
+    fetch(`${apiUrl}/api/tasks/${currentTaskId}/`, {
         method: 'PATCH',
         headers: {
             'Authorization': `Token ${token}`,
